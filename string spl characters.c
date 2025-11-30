ace2879ace043a473bf797dec0201e45421a787d
sahithipindiproli <sahithipindiproli@gmail.com>
2025-11-30 15:49:32 +0530

    Add files via upload

---
+++ string spl characters.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char txt[] = "It\'s alright.";
+  printf("%s\n", txt);
+  char etxt[] = "We are the so-called \"Vikings\" from the north.";
+  printf("%s", etxt);
+ 
+ 
+  return 0;
+}
+
+
---
+++ strings ex.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char greetings[] = "Hello World!";
+  int i;
+  int n=12;
+  for(i=0;i<n;i++){
+		
+  printf("%c\n", greetings[i]);}
+ 
+  return 0;
+}
